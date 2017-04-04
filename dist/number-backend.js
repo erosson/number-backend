de1cacf8fd6f4846a77369ba25949858a920b7fb
Evan <github@erosson.org>
2017-04-04 15:34:08 +0100

    0.0.5

--- dist/number-backend.js
+++ dist/number-backend.js
@@ -64,6 +64,16 @@ return /******/ (function(modules) { // webpackBootstrap
 	
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 	
+	function isWrapped(a) {
+	  return a && typeof a.val !== 'undefined';
+	}
+	function unwrap(a) {
+	  return isWrapped(a) ? a.val : a;
+	}
+	function wrap(a) {
+	  return isWrapped(a) ? a : new WrapNative(a);
+	}
+	
 	var WrapNative = function () {
 	  function WrapNative(val) {
 	    _classCallCheck(this, WrapNative);
@@ -74,39 +84,14 @@ return /******/ (function(modules) { // webpackBootstrap
 	
 	
 	  _createClass(WrapNative, [{
-	    key: 'add',
-	    value: function add(a) {
-	      return new WrapNative(this.val + a);
-	    }
-	  }, {
-	    key: 'div',
-	    value: function div(a) {
-	      return new WrapNative(this.val / a);
-	    }
-	  }, {
-	    key: 'mod',
-	    value: function mod(a) {
-	      return new WrapNative(this.val % a);
-	    }
-	  }, {
-	    key: 'mul',
-	    value: function mul(a) {
-	      return new WrapNative(this.val * a);
-	    }
-	  }, {
-	    key: 'pow',
-	    value: function pow(a) {
-	      return new WrapNative(Math.pow(this.val, a));
-	    }
-	  }, {
-	    key: 'sub',
-	    value: function sub(a) {
-	      return new WrapNative(this.val - a);
-	    }
-	  }, {
 	    key: 'abs',
 	    value: function abs() {
 	      return new WrapNative(Math.abs(this.val));
+	    }
+	  }, {
+	    key: 'absoluteValue',
+	    value: function absoluteValue() {
+	      return this.abs();
 	    }
 	  }, {
 	    key: 'ceil',
@@ -119,24 +104,117 @@ return /******/ (function(modules) { // webpackBootstrap
 	      return new WrapNative(Math.floor(this.val));
 	    }
 	  }, {
-	    key: 'min',
-	    value: function min() {
-	      return new WrapNative(Math.min(this.val));
+	    key: 'neg',
+	    value: function neg() {
+	      return new WrapNative(this.val * -1);
 	    }
 	  }, {
-	    key: 'max',
-	    value: function max() {
-	      return new WrapNative(Math.max(this.val));
+	    key: 'negated',
+	    value: function negated() {
+	      return this.neg();
 	    }
 	  }, {
 	    key: 'round',
 	    value: function round() {
 	      return new WrapNative(Math.round(this.val));
 	    }
+	
+	    // toDecimalPlaces
+	    // toDP
+	    // toFraction
+	    // toNearest
+	    // toSignificantDigits
+	    // toSD
+	
 	  }, {
 	    key: 'trunc',
 	    value: function trunc() {
 	      return new WrapNative(Math.trunc(this.val));
+	    }
+	  }, {
+	    key: 'truncated',
+	    value: function truncated() {
+	      return this.trunc();
+	    }
+	
+	    // toBinary
+	    // toExponential
+	    // toFixed
+	    // toHex
+	    // toHexadecimal
+	
+	  }, {
+	    key: 'toJson',
+	    value: function toJson() {
+	      return this.val;
+	    }
+	    // toOctal
+	    // toPrecision
+	
+	  }, {
+	    key: 'toString',
+	    value: function toString() {
+	      return this.val + '';
+	    }
+	  }, {
+	    key: 'valueOf',
+	    value: function valueOf() {
+	      return this.val;
+	    }
+	    // toNumber
+	
+	    // cmp
+	    // comparedTo
+	
+	  }, {
+	    key: 'eq',
+	    value: function eq(a) {
+	      return this.val === unwrap(a);
+	    }
+	  }, {
+	    key: 'equals',
+	    value: function equals(a) {
+	      return this.eq(a);
+	    }
+	  }, {
+	    key: 'gt',
+	    value: function gt(a) {
+	      return this.val > unwrap(a);
+	    }
+	  }, {
+	    key: 'greaterThan',
+	    value: function greaterThan(a) {
+	      return this.gt(a);
+	    }
+	  }, {
+	    key: 'gte',
+	    value: function gte(a) {
+	      return this.val >= unwrap(a);
+	    }
+	  }, {
+	    key: 'greaterThanOrEqualTo',
+	    value: function greaterThanOrEqualTo(a) {
+	      return this.gte(a);
+	    }
+	  }, {
+	    key: 'lt',
+	    value: function lt(a) {
+	      return this.val < unwrap(a);
+	    }
+	  }, {
+	    key: 'lessThan',
+	    value: function lessThan(a) {
+	      return this.lt(a);
+	    }
+	  }, {
+	    key: 'lte',
+	    value: function lte(a) {
+	      return this.val <= unwrap(a);
+	    }
+	  }, {
+	    key: 'lessThanOrEqualTo',
+	    value: function lessThanOrEqualTo(a) {
+	      return this.lte(a);
 	    }
 	  }, {
 	    key: 'cbrt',
@@ -144,9 +222,59 @@ return /******/ (function(modules) { // webpackBootstrap
 	      return new WrapNative(Math.cbrt(this.val));
 	    }
 	  }, {
-	    key: 'sqrt',
-	    value: function sqrt() {
-	      return new WrapNative(Math.sqrt(this.val));
+	    key: 'cubeRoot',
+	    value: function cubeRoot() {
+	      return this.cbrt();
+	    }
+	  }, {
+	    key: 'div',
+	    value: function div(a) {
+	      return new WrapNative(this.val / unwrap(a));
+	    }
+	  }, {
+	    key: 'dividedBy',
+	    value: function dividedBy(a) {
+	      return this.div(a);
+	    }
+	  }, {
+	    key: 'divToInt',
+	    value: function divToInt(a) {
+	      return this.div(a).floor();
+	    }
+	  }, {
+	    key: 'dividedToIntegerBy',
+	    value: function dividedToIntegerBy(a) {
+	      return this.divToInt();
+	    }
+	  }, {
+	    key: 'log',
+	    value: function log(n) {
+	      return new WrapNative(Math.log(this.val, unwrap(n)));
+	    }
+	  }, {
+	    key: 'logarithm',
+	    value: function logarithm(n) {
+	      return this.log(n);
+	    }
+	  }, {
+	    key: 'sub',
+	    value: function sub(a) {
+	      return new WrapNative(this.val - unwrap(a));
+	    }
+	  }, {
+	    key: 'minus',
+	    value: function minus(a) {
+	      return this.sub(a);
+	    }
+	  }, {
+	    key: 'mod',
+	    value: function mod(a) {
+	      return new WrapNative(this.val % unwrap(a));
+	    }
+	  }, {
+	    key: 'modulo',
+	    value: function modulo(a) {
+	      return this.mod(a);
 	    }
 	  }, {
 	    key: 'exp',
@@ -154,15 +282,130 @@ return /******/ (function(modules) { // webpackBootstrap
 	      return new WrapNative(Math.exp(this.val));
 	    }
 	  }, {
+	    key: 'naturalExponential',
+	    value: function naturalExponential() {
+	      return this.exp();
+	    }
+	  }, {
 	    key: 'ln',
 	    value: function ln() {
 	      return new WrapNative(Math.ln(this.val));
 	    }
 	  }, {
-	    key: 'log',
-	    value: function log() {
-	      return new WrapNative(Math.log(this.val));
+	    key: 'naturalLogarithm',
+	    value: function naturalLogarithm() {
+	      return this.ln();
 	    }
+	  }, {
+	    key: 'add',
+	    value: function add(a) {
+	      return new WrapNative(this.val + unwrap(a));
+	    }
+	  }, {
+	    key: 'plus',
+	    value: function plus(a) {
+	      return this.add(a);
+	    }
+	  }, {
+	    key: 'sqrt',
+	    value: function sqrt() {
+	      return new WrapNative(Math.sqrt(this.val));
+	    }
+	  }, {
+	    key: 'squareRoot',
+	    value: function squareRoot() {
+	      return this.sqrt();
+	    }
+	  }, {
+	    key: 'mul',
+	    value: function mul(a) {
+	      return new WrapNative(this.val * unwrap(a));
+	    }
+	  }, {
+	    key: 'times',
+	    value: function times(a) {
+	      return this.mul(a);
+	    }
+	  }, {
+	    key: 'pow',
+	    value: function pow(a) {
+	      return new WrapNative(Math.pow(this.val, unwrap(a)));
+	    }
+	  }, {
+	    key: 'toPower',
+	    value: function toPower(a) {
+	      return this.pow(a);
+	    }
+	
+	    // dp
+	    // decimalPlaces
+	    // sd
+	    // precision
+	
+	  }, {
+	    key: 'cos',
+	    value: function cos() {
+	      return new WrapNative(Math.cos(this.val));
+	    }
+	  }, {
+	    key: 'cosine',
+	    value: function cosine() {
+	      return this.cos();
+	    }
+	  }, {
+	    key: 'sin',
+	    value: function sin() {
+	      return new WrapNative(Math.sin(this.val));
+	    }
+	  }, {
+	    key: 'sine',
+	    value: function sine() {
+	      return this.sin();
+	    }
+	  }, {
+	    key: 'tan',
+	    value: function tan() {
+	      return new WrapNative(Math.tan(this.val));
+	    }
+	  }, {
+	    key: 'tangent',
+	    value: function tangent() {
+	      return this.tan();
+	    }
+	  }, {
+	    key: 'acos',
+	    value: function acos() {
+	      return new WrapNative(Math.acos(this.val));
+	    }
+	  }, {
+	    key: 'inverseCosine',
+	    value: function inverseCosine() {
+	      return this.acos();
+	    }
+	  }, {
+	    key: 'asin',
+	    value: function asin() {
+	      return new WrapNative(Math.asin(this.val));
+	    }
+	  }, {
+	    key: 'inverseSine',
+	    value: function inverseSine() {
+	      return this.asin();
+	    }
+	  }, {
+	    key: 'atan',
+	    value: function atan() {
+	      return new WrapNative(Math.atan(this.val));
+	    }
+	  }, {
+	    key: 'inverseTangent',
+	    value: function inverseTangent() {
+	      return this.asin();
+	    }
+	
+	    // cosh...
+	    // acosh...
+	
 	  }, {
 	    key: 'log2',
 	    value: function log2() {
@@ -173,41 +416,6 @@ return /******/ (function(modules) { // webpackBootstrap
 	    value: function log10() {
 	      return new WrapNative(Math.log10(this.val));
 	    }
-	  }, {
-	    key: 'cos',
-	    value: function cos() {
-	      return new WrapNative(Math.cos(this.val));
-	    }
-	  }, {
-	    key: 'sin',
-	    value: function sin() {
-	      return new WrapNative(Math.sin(this.val));
-	    }
-	  }, {
-	    key: 'tan',
-	    value: function tan() {
-	      return new WrapNative(Math.tan(this.val));
-	    }
-	  }, {
-	    key: 'acos',
-	    value: function acos() {
-	      return new WrapNative(Math.acos(this.val));
-	    }
-	  }, {
-	    key: 'asin',
-	    value: function asin() {
-	      return new WrapNative(Math.asin(this.val));
-	    }
-	  }, {
-	    key: 'atan',
-	    value: function atan() {
-	      return new WrapNative(Math.atan(this.val));
-	    }
-	  }, {
-	    key: 'atan2',
-	    value: function atan2() {
-	      return new WrapNative(Math.atan2(this.val));
-	    }
 	  }]);
 	
 	  return WrapNative;
@@ -216,7 +424,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	var backends = {
 	  'native': {
 	    normalize: function normalize(val) {
-	      return val;
+	      return wrap(val);
 	    }
 	  },
 	
