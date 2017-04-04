93e3f5c16ff49399452b8ea247fd22c2d6f6cce9
Evan <github@erosson.org>
2017-04-04 14:39:43 +0100

    operators

--- src/number-backend.js
+++ src/number-backend.js
@@ -1,3 +1,41 @@
+class WrapNative {
+  constructor(val) {
+    this.val = val
+  }
+  // match the decimal.js interface. https://github.com/MikeMcl/decimal.js/
+  add(a) { return new WrapNative(this.val + a) }
+  div(a) { return new WrapNative(this.val / a) }
+  mod(a) { return new WrapNative(this.val % a) }
+  mul(a) { return new WrapNative(this.val * a) }
+  pow(a) { return new WrapNative(Math.pow(this.val, a)) }
+  sub(a) { return new WrapNative(this.val - a) }
+
+  abs() { return new WrapNative(Math.abs(this.val)) }
+  ceil() { return new WrapNative(Math.ceil(this.val)) }
+  floor() { return new WrapNative(Math.floor(this.val)) }
+  min() { return new WrapNative(Math.min(this.val)) }
+  max() { return new WrapNative(Math.max(this.val)) }
+  round() { return new WrapNative(Math.round(this.val)) }
+  trunc() { return new WrapNative(Math.trunc(this.val)) }
+
+  cbrt() { return new WrapNative(Math.cbrt(this.val)) }
+  sqrt() { return new WrapNative(Math.sqrt(this.val)) }
+
+  exp() { return new WrapNative(Math.exp(this.val)) }
+  ln() { return new WrapNative(Math.ln(this.val)) }
+  log() { return new WrapNative(Math.log(this.val)) }
+  log2() { return new WrapNative(Math.log2(this.val)) }
+  log10() { return new WrapNative(Math.log10(this.val)) }
+
+  cos() { return new WrapNative(Math.cos(this.val)) }
+  sin() { return new WrapNative(Math.sin(this.val)) }
+  tan() { return new WrapNative(Math.tan(this.val)) }
+  acos() { return new WrapNative(Math.acos(this.val)) }
+  asin() { return new WrapNative(Math.asin(this.val)) }
+  atan() { return new WrapNative(Math.atan(this.val)) }
+  atan2() { return new WrapNative(Math.atan2(this.val)) }
+}
+
 const backends = {
   'native': {
     normalize(val) {
